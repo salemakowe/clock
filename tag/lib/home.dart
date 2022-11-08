df48588174b4fabb5c87c7c1ae56fe1f3f8ca877
Salem Akowe <salemakowe@gmail.com>
2022-11-08 15:51:21 +0100

    .

--- tag/lib/home.dart
+++ tag/lib/home.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import "package:get/get.dart";
+
+import 'widgets/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,34 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.lightbulb),
+              onPressed: () {
+                Get.isDarkMode
+                    ? Get.changeTheme(Themes.lightMode)
+                    : Get.changeTheme(Themes.darkMode);
+              })
+        ],
+        title: const Text(
+          "Tag",
+          style: TextStyle(
+            fontFamily: "Orlande",
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+
+      //drawer
+
+      //body
+
+      //floating action button
+    );
   }
 }
