9467ddc106bd592f7930f88de3e7674df250e90d
Salem Akowe <salemakowe@gmail.com>
2023-04-12 02:07:40 +0100

    .

---
+++ wakky/lib/clockview.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+
+class ClockView extends StatefulWidget {
+  const ClockView({super.key});
+
+  @override
+  State<ClockView> createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.4,
+      color: Colors.amber,
+      child: CustomPaint(
+        painter: ClockPainter(),
+      ),
+    );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- wakky/lib/homepage.dart
+++ wakky/lib/homepage.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:wakky/clockview.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,25 +12,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            Color(0xff646e82),
-            Color(0xffffff),
-            Color(0xe9edf0),
-            Color(0xa6b4c8)
-          ],
-        )),
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.4,
-              color: Colors.blueGrey,
-            ),
-          ],
-        ),
+      body: Column(
+        children: const [
+          ClockView(),
+        ],
       ),
     );
   }
