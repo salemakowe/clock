1322d576eccfb54db7386884ae66b4df03eedfda
Salem Akowe <salemakowe@gmail.com>
2023-04-16 11:24:06 +0100

    .

--- wakky/lib/homepage.dart
+++ wakky/lib/homepage.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:wakky/clockview.dart";
+import 'package:wakky/widgets/analog_clockview.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,21 +12,57 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade300,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
+      backgroundColor: const Color.fromARGB(255, 247, 247, 247),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.transparent,
+      // ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          GestureDetector(child: const ClockView()),
+          const SizedBox(height: 10),
+          GestureDetector(
+            onTap: () {
+              print("pressed");
+            },
+            child: const ClockView(),
+          ),
+          const SizedBox(height: 10),
           Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Column(
-                children: [
-                  Row(),
-                ],
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                color: Color.fromARGB(255, 228, 226, 226),
+              ),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("Alarm"),
+                        Text("Alarm"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            height: MediaQuery.of(context).size.height / 10,
+                            decoration: BoxDecoration(color: Colors.red),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- wakky/lib/widgets/analog_clockview.dart
+++ wakky/lib/widgets/analog_clockview.dart
@@ -21,10 +21,10 @@ class _ClockViewState extends State<ClockView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.4,
-      color: Colors.amber,
+      //color: Colors.amber,
 
       //we need to put the CustomPaint widget in a Transform widget and
       //rotate the children anticlockwise by 90deg so the widget will assum a real life clock position
@@ -53,33 +53,48 @@ class ClockPainter extends CustomPainter {
     // paints for the circles
     var innerCircle = Paint()..color = const Color(0xff444975);
     var outterCircle = Paint()
-      ..color = const Color(0xffeaecff) //the color of the outter circle
+      ..color = const Color.fromARGB(
+          255, 200, 205, 248) //the color of the outter circle
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 12; // the width of the brush for outter circle
+      ..strokeWidth = 25; // the width of the brush for outter circle
 
     var smallInner = Paint()..color = const Color(0xffeaecff);
+    var dashBrush = Paint()..color = Colors.black;
+
+    var outterCircleRadius = radius;
+    var innerCircleRadius = radius - 14;
+    for (double i = 0; i < 360; i += 90) {
+      var x1 = centerX + outterCircleRadius * cos(i * pi / 180);
+      var y1 = centerY + outterCircleRadius * sin(i * pi / 180);
+
+      var x2 = centerX + innerCircleRadius * cos(i * pi / 180);
+      var y2 = centerY + innerCircleRadius * sin(i * pi / 180);
+
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
+    }
 
     // second, minuite and hour hand brush
     // second handBrush
     var secHand = Paint()
-      ..color = const Color(0xffffcc80)
+      ..color = Colors.red
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round //make the rectangle shape round
       ..strokeWidth = 4;
 
     // minute handBrush
     var minHand = Paint()
-      ..shader = const RadialGradient(colors: [Colors.lightBlue, Colors.pink])
-          .createShader(Rect.fromCircle(center: center, radius: radius))
+      // ..shader = const RadialGradient(colors: [Colors.black45, Colors.black87])
+      //     .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..color = Colors.black
       ..style = PaintingStyle.stroke
       ..strokeWidth = 6
       ..strokeCap = StrokeCap.round;
 
     // hour handBrush
     var hourHand = Paint()
-      ..shader = const RadialGradient(
-              colors: [Color.fromARGB(255, 202, 68, 255), Colors.orange])
-          .createShader(Rect.fromCircle(center: center, radius: radius))
+      // ..shader = const RadialGradient(colors: [Colors.black54, Colors.black87])
+      //     .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..color = Colors.black
       ..style = PaintingStyle.stroke
       ..strokeWidth = 12
       ..strokeCap = StrokeCap.round;
@@ -89,26 +104,25 @@ class ClockPainter extends CustomPainter {
     canvas.drawCircle(center, radius - 40, outterCircle);
     canvas.drawCircle(center, radius - 40, innerCircle);
 
-    //calculation for the hour hand
-    //note, bcos we need to convert the values from their original degree values to radiant (pi/180)
-    var hourHandX = centerX +
-        30 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
-    var hourHandY = centerY +
-        30 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
-
-    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHand);
-
-    //calculation for the minute hand
-    //note, bcos we need to convert the values from their original degree values to radiant (pi/180)
-    var minHandX = centerX + 50 * cos(dateTime.minute * 6 * pi / 180);
-    var minHandY = centerY + 50 * sin(dateTime.minute * 6 * pi / 180);
-    canvas.drawLine(center, Offset(minHandX, minHandY), minHand);
-
     //calculation for the seconds hand
     //note, bcos we need to convert the values from their original degree values to radiant (pi/180)
     var secHandX = centerX + 70 * cos(dateTime.second * 6 * pi / 180);
     var secHandY = centerY + 70 * sin(dateTime.second * 6 * pi / 180);
     canvas.drawLine(center, Offset(secHandX, secHandY), secHand);
+
+    //calculation for the minute hand
+    //note, bcos we need to convert the values from their original degree values to radiant (pi/180)
+    var minHandX = centerX + 60 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY = centerY + 60 * sin(dateTime.minute * 6 * pi / 180);
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHand);
+
+    //calculation for the hour hand
+    //note, bcos we need to convert the values from their original degree values to radiant (pi/180)
+    var hourHandX = centerX +
+        40 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hourHandY = centerY +
+        40 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHand);
 
     //positioned last cos its ontop of the sec, min and hour hand
     canvas.drawCircle(center, 10, smallInner);
